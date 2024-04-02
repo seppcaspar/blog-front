8f11d1188b59bd1d32b340af831a37f62692c9e9
seppcaspar <caspar.sepp@tptlive.ee>
2024-04-02 13:46:07 +0300

    register lisatud

---
+++ src/components/MainNav.vue
@@ -0,0 +1,33 @@
+<template>
+    <b-navbar>
+        <template #brand>
+            <b-navbar-item tag="router-link" :to="{ path: '/' }">
+                <img
+                    src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png"
+                    alt="Lightweight UI components for Vue.js based on Bulma"
+                >
+            </b-navbar-item>
+        </template>
+        <template #start>
+            <b-navbar-item tag="router-link" to="/">
+                Home
+            </b-navbar-item>
+            <b-navbar-item tag="router-link" to="/todo">
+                Todo
+            </b-navbar-item>
+        </template>
+
+        <template #end>
+            <b-navbar-item tag="div">
+                <div class="buttons">
+                    <a class="button is-primary">
+                        <strong>Sign up</strong>
+                    </a>
+                    <RouterLink to="/login" class="button is-light">
+                        Log in
+                    </RouterLink>
+                </div>
+            </b-navbar-item>
+        </template>
+    </b-navbar>
+</template>
---
+++ src/main.js
@@ -0,0 +1,33 @@
+import { createApp } from 'vue';
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router';
+
+import './style.css';
+import Buefy from '@ntohq/buefy-next';
+import '@ntohq/buefy-next/dist/buefy.css';
+
+import App from './App.vue';
+
+import Home from './pages/Home.vue';
+import ToDo from './pages/ToDo.vue';
+import Login from './pages/Login.vue';
+// 2. Define some routes
+// Each route should map to a component.
+// We'll talk about nested routes later.
+const routes = [
+    { path: '/', component: Home },
+    { path: '/todo', component: ToDo },
+    { path: '/login', component: Login },
+];
+// 3. Create the router instance and pass the `routes` option
+// You can pass in additional options here, but let's
+// keep it simple for now.
+const router = createRouter({
+    // 4. Provide the history implementation to use. We
+    // are using the hash history for simplicity here.
+    history: createWebHistory(),
+    routes, // short for `routes: routes`
+});
+const app = createApp(App);
+app.use(router);
+app.use(Buefy);
+app.mount('#app');
